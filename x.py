df5c07cd558e59af381b4e2b0f7e8ba3ca464b9e
Valeria Moreno <91974128+valmoreno28@users.noreply.github.com>
2022-04-18 09:01:31 -0600

    Update x.py

--- x.py
+++ x.py
@@ -1,21 +1,15 @@
 import streamlit as st
-import numpy as np 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import censusdata
 
 
-fill_in_the_blanks = {'questions': ['question1','question2'], 'answers': ['answer1','answer2']}
-ans = []
-mark = 0
-with st.form(key = id_generator()):
-	for i,quest in enumerate(fill_in_the_blanks["questions"]):
-		st.write(i+1,quest)
-		x = st.text_input("Enter your answer here",key=id_generator())
-		ans.append(x)
-	submitted = st.form_submit_button(label='Submit')
-	if submitted:
-		# if answers are correct then
-			mark = mark+1
-	st.success("Test Score - "+str(mark))
+
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
