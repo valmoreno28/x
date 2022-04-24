f0d7c6a5592ff679b68b48c6235f861f78871cf9
Valeria Moreno <91974128+valmoreno28@users.noreply.github.com>
2022-04-24 16:13:00 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,8 +1,8 @@
 import streamit as st
 
 choices2 = [({'Hombre'}, suma = 0), ({'Mujer'}, suma= suma + 2)]
-choices3 = [({'No'}, suma = 0), ({'Si'}, suma = suma + 2)]
-choices4 = [({'No'}, suma = 0), ({'Si'}, suma = suma + 2)]
+choices3 = [({'No'}, suma1 = 0), ({'Si'}, suma1 = suma + 2)]
+choices4 = [({'No'}, suma2 = 0), ({'Si'}, suma2 = suma + 2)]
 
 qs2 = [('¿Cual fue tu sexo asignado al nacer?', choices2),
 ('sexo biologico', choices2),
