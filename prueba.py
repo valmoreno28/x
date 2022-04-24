0cf3aa83747a05ca48b069847a17aea076ee1de6
Valeria Moreno <91974128+valmoreno28@users.noreply.github.com>
2022-04-24 16:28:03 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,20 +6,20 @@ def main():
 		placeholder = st.empty()
 	num = st.session_state.num
 with placeholder.form(key=str(num)):
-	st.radio(qs1[num][0], key=num+1, options=qs1[num][1])
-	st.radio(qs2[num][0], key=num+1, options=qs2[num][1])
-	st.radio(qs3[num][0], key=num+1, options=qs3[num][1])
+	st.radio(qs1[num][0], key=num+1, options=qs2[num][1])
+	st.radio(qs2[num][0], key=num+1, options=qs3[num][1])
+	st.radio(qs3[num][0], key=num+1, options=qs4[num][1])
 
 
 qs2 = [('¿Cual fue tu sexo asignado al nacer?', choices2),
 ('sexo biologico', choices2),
 ('sexo biologico', choices2)]
-choices2 = ['Hombre', suma = 0, 'Mujer', suma= suma + 2]
+choices2 = [('Hombre', suma = 0), ('Mujer', suma2= 2)]
 
 qs3 = [('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
 ('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
 ('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3)]
-choices3 = ['No', suma1 = 0, 'Si', suma1 = suma1 + 2]
+choices3 = ['No', suma1 = 0, 'Si', suma1 = suma2 + 2]
 
 qs4 = [('¿Haz practicado sexo sin condón?', choices4),
 ('¿Haz practicado sexo sin condón?', choices4),
