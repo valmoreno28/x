e7516c80ebffa16761d3b8a1cb73b71e1e975e9a
Valeria Moreno <91974128+valmoreno28@users.noreply.github.com>
2022-04-24 16:14:56 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,19 +1,5 @@
 import streamit as st
 
-choices2 = [({'Hombre'}, suma = 0), ({'Mujer'}, suma= suma + 2)]
-choices3 = [({'No'}, suma1 = 0), ({'Si'}, suma1 = suma + 2)]
-choices4 = [({'No'}, suma2 = 0), ({'Si'}, suma2 = suma + 2)]
-
-qs2 = [('¿Cual fue tu sexo asignado al nacer?', choices2),
-('sexo biologico', choices2),
-('sexo biologico', choices2)]
-qs3 = [('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
-('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
-('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3)]
-qs4 = [('¿Haz practicado sexo sin condón?', choices4),
-('¿Haz practicado sexo sin condón?', choices4),
-('¿Haz practicado sexo sin condón?', choices4)]
-
 
 def main():
 	for _,  _, _ in zip(qs2, qs3, qs4):
@@ -23,5 +9,22 @@ with placeholder.form(key=str(num)):
 	st.radio(qs1[num][0], key=num+1, options=qs1[num][1])
 	st.radio(qs2[num][0], key=num+1, options=qs2[num][1])
 	st.radio(qs3[num][0], key=num+1, options=qs3[num][1])
+
+
+qs2 = [('¿Cual fue tu sexo asignado al nacer?', choices2),
+('sexo biologico', choices2),
+('sexo biologico', choices2)]
+choices2 = [({'Hombre'}, suma = 0), ({'Mujer'}, suma= suma + 2)]
+
+qs3 = [('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
+('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3),
+('¿Haz practicado alguna vez sexo sin metodos anticonceptivos fisicos como el condón?', choices3)]
+choices3 = [({'No'}, suma1 = 0), ({'Si'}, suma1 = suma + 2)]
+
+qs4 = [('¿Haz practicado sexo sin condón?', choices4),
+('¿Haz practicado sexo sin condón?', choices4),
+('¿Haz practicado sexo sin condón?', choices4)]
+choices4 = [({'No'}, suma2 = 0), ({'Si'}, suma2 = suma + 2)]
+
 	
 	
